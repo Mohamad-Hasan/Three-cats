dbe986c51bf723e7a13a7c960285efb4aa892790
Mohamad <ph.mohamad.h@gmail.com>
2020-12-13 15:47:19 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let today = new Date();
+let year = today.getFullYear();
+let mariamAge = year - 2013;
+
+let aboudaAge = year - 2016;
+
+let jojoAge = year - 2018;
+
+document.getElementById("mariamAge").innerHTML += " " + mariamAge;
+document.getElementById("aboudaAge").innerHTML += " " + aboudaAge;
+document.getElementById("jojoAge").innerHTML += " " + jojoAge;
+
